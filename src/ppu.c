e412a0403f1ac06227cdc18ac77a24341b9a4f04
Andrew Newbill <fidolimp@gmail.com>
2023-06-11 04:04:25 +0000

    Did a bunch of stuff related to interrupt handling. Not even close to done yet.

--- src/ppu.c
+++ src/ppu.c
@@ -16,3 +16,22 @@ int get_lcd_ly(cpu_state *state)
 {
     return state->address_space[0xff44];
 }
+
+void ppu_cycle(cpu_state * _cpu_state, ppu_state * _ppu_state)
+{
+    _ppu_state->lcd_ly++;
+    _cpu_state->address_space[0xff44] = _ppu_state->lcd_ly;
+
+    _ppu_state->ly_comp = _cpu_state->address_space[0xff45];
+
+    if(_ppu_state->lcd_ly == _ppu_state->ly_comp)
+    {
+        _cpu_state->address_space[0xff41] = setBit(_cpu_state->address_space[0xff41], 2);//Set LYC=LY FLAG
+        
+
+        if(checkBit(_cpu_state->address_space[0xff41], ))
+        {
+            
+        }
+    } 
+}
